562001c6bc2d66afe1ca3c095a67c7a184c1c9c9
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-15 22:28:29 +0200

    Add files via upload

---
+++ Task02A_Pascal_Triangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task02A_Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            int numberOfElements = 0;
+
+            for (int i = 0; i <= lines; i++)
+            {
+                numberOfElements += i;
+            }
+
+            int[] pasclaTriangle = new int[numberOfElements];
+
+
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
