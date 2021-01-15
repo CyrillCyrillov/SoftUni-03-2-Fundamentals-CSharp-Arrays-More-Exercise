c5c72eb5e3f1142efc3f79c77ec36aa8cb2728e2
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-15 22:29:20 +0200

    Add files via upload

---
+++ Task03_Recursive_Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task03_Recursive_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfFibonacci = int.Parse(Console.ReadLine());
+
+            int result = 1;
+            int previous = 1;
+            int previousPrevious = 1;
+            if (numberOfFibonacci > 2)
+            {
+                for (int i = 3; i <= numberOfFibonacci; i++)
+                {
+                    result = previous + previousPrevious;
+                    previousPrevious = previous;
+                    previous = result;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
