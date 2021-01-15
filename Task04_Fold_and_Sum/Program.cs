99fde0a64f6ffb8769df92911f6b93011f23d211
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-15 22:30:22 +0200

    Add files via upload

---
+++ Task04_Fold_and_Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Task04_Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int lenght = numbers.Length;
+            int helpIndex = lenght / 4 - 1;
+
+            for (int i = (lenght / 4); i <= (lenght / 2 - 1); i++)
+            {
+                Console.Write($"{numbers[i] + numbers[helpIndex]} ");
+                helpIndex--;
+            }
+
+            helpIndex = lenght - 1;
+            for (int i = (lenght / 2); i <= (3 * lenght / 4 -1) ; i++)
+            {
+                Console.Write($"{numbers[i] + numbers[helpIndex]} ");
+                helpIndex--;
+            }
+        }
+    }
+}
