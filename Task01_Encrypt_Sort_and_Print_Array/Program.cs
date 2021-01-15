b0f102d07c1692a8e4bbd88f57f1d449b33d08c5
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-15 22:27:00 +0200

    Add files via upload

---
+++ Task01_Encrypt_Sort_and_Print_Array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _Task01_Encrypt_Sort_and_Print_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nameNumber = int.Parse(Console.ReadLine());
+            
+            int[] codeNames = new int[nameNumber];
+            
+           
+            for (int i = 0; i < nameNumber; i++)
+            {
+                string name = Console.ReadLine();
+                
+                int vowelSum = 0;
+                int consonantSum = 0;
+                string vowelLetters = "AaEeOoIiUu";
+                for (int j = 0; j < name.Length; j++)
+                {
+                    char nextLetter = name[j];
+                    if (vowelLetters.Contains(nextLetter))
+                    {
+                        vowelSum += nextLetter * name.Length;
+                    }
+                    else
+                    {
+                        consonantSum += nextLetter / name.Length;
+                    }
+                }
+                codeNames[i] = vowelSum + consonantSum;
+            }
+
+            Array.Sort(codeNames);
+
+            for (int i = 0; i < nameNumber; i++)
+            {
+                Console.WriteLine(codeNames[i]);
+            }
+        }
+    }
+}
